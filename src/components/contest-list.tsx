6b10c243dfaf0ae297215c187d097c4a9cc55d4b
Ehijie <eaghedo1@gmail.com>
2024-01-03 15:37:24 +0000

    fetching data while react is rendering rather than before

--- src/components/contest-list.tsx
+++ src/components/contest-list.tsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
+import { fetchContests } from "../api-client";
+
 import ContestPreview from "./contest-preview";
 
-const ContestList = ({ contests }) => {
+const ContestList = ({ initialContests }) => {
+  const [contests, setContests] = useState(initialContests);
+
+  useEffect(() => {
+    fetchContests().then((data) => {
+      // state
+      console.log(data.contests);
+      setContests(data.contests);
+    });
+  }, []);
   return (
     <div className="contest-list">
       {contests.map((contest) => {
-        return <ContestPreview contest={contest} />;
+        return (
+          <ContestPreview key={contest.id} contest={contest} />
+        );
       })}
     </div>
   );
