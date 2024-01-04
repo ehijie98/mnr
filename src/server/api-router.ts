d51192c0525a9a0ea34266006b9d8d0baf338122
Ehijie <eaghedo1@gmail.com>
2024-01-04 16:01:31 +0000

    now fetching data from mongodb and not mock data

--- src/server/api-router.ts
+++ src/server/api-router.ts
@@ -1,14 +1,25 @@
 import express from "express";
 import cors from "cors";
 
+import { connectClient } from "./db";
+
 const router = express.Router();
 router.use(cors());
 
-import testData from "../test-data.json";
-
-router.get("/contests", (req, res) => {
+router.get("/contests", async (req, res) => {
   // get the data from MongoDB
-  res.send({ contests: testData });
+  const client = await connectClient();
+
+  const contestsArray = await client
+    .collection("contests")
+    .find()
+    .project({
+      id: 1,
+      categoryName: 1,
+      contestName: 1,
+    })
+    .toArray();
+  res.send({ contests: contestsArray });
 });
 
 // router.get("/contest")
